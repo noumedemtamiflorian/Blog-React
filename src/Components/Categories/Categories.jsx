b5838eeedd6c23e8d35cc555e62f260d69d4994c
noumedem <noumedemtamiflorian@gmail.com>
2021-04-28 14:56:32 +0200

    Ajout de l'image par default

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { Component } from 'react'
-import { Link } from 'react-router-dom';
+import { Pagination } from "antd"
 import Header from '../Header/Header';
 import Posts from '../Posts/Posts';
+import 'antd/dist/antd.css';
+import { Link } from 'react-router-dom';
+
 class Categories extends Component {
     constructor(props) {
         super(props);
@@ -10,32 +13,40 @@ class Categories extends Component {
             posts: [],
             categories: [],
             id: this.props.match.params.id,
-            categorie: { title: '' }
+            categorie: { title: '' },
+            eltPage: 9,
+            pageCurrent: 1,
+            data: []
         }
     }
+
+    handleChange = value => {
+        const { eltPage } = this.state;
+        const indexOfLastLog = value * eltPage;
+        const indexOfFirstLog = indexOfLastLog - eltPage;
+        let data = this.state.data
+        this.setState({
+            currentPage: value,
+            posts: data.slice(indexOfFirstLog, indexOfLastLog)
+        });
+    };
     componentDidMount() {
         const id = this.state.id;
         axios.get("https://localhost:8000/api/categories/" + id)
             .then(res => {
-                console.log("dd", res.data);
                 this.setState({
-                    categorie: res.data
-                })
-            }).catch()
-        axios.get("https://localhost:8000/api/articles?categorie=" + id)
-            .then(res => {
-                let donnes = res.data['hydra:member']
-                this.setState({
-                    posts: donnes
+                    categorie: res.data,
+                    posts: res.data.articles.slice(0, this.state.eltPage),
+                    data: res.data.articles
                 })
             }).catch()
         axios.get("https://127.0.0.1:8000/api/categories")
             .then(res => {
-                console.log(res.data);
                 let donnes = res.data['hydra:member']
                 this.setState({
                     categories: donnes
                 })
+                console.log(donnes);
             }).catch()
     }
 
@@ -43,9 +54,9 @@ class Categories extends Component {
         return (
             <div>
                 <Header />
-                <h1 className="text-center"> Article pour la categorie {this.state.categorie.title}</h1>
+                <h1 className="text-center"> Articles pour la categorie {this.state.categorie.title}</h1>
                 <main className="row">
-                    <article className="col-sm-7 col-md-8 col-lg-9 col-xl-10">
+                    <article className="col-10  m-auto">
                         <Posts posts={this.state.posts} />
                     </article>
                     <div id="categories" className="col-sm-5 col-md-4  col-lg-3 col-xl-2">
@@ -53,13 +64,21 @@ class Categories extends Component {
                         <div className="list-group mr-4">
                             {
                                 this.state.categories.map(({ title, id }, index) => {
-                                    return <Link key={id} to={"/posts/categorie/" + id}><span id={id}
-                                        className={"list-group-item list-group-item-action"}>{title}</span></Link>
+                                    return <Link key={id} to={"/posts/categorie/" + id}><span id={id} className="list-group-item list-group-item-action">{title}</span></Link>
                                 })
                             }
                         </div>
                     </div>
                 </main>
+                <footer className="pagination_div">
+                    <Pagination
+                        defaultCurrent={this.state.pageCurrent}
+                        defaultPageSize={this.state.eltPage} //default size of page
+                        pageSize={this.state.eltPage}
+                        onChange={this.handleChange}
+                        total={/*loadingOk && */this.state.data.length > 0 && this.state.data.length} //total number of card data available
+                    />
+                </footer>
             </div>
         )
     }
--- src/Components/Posts/Post.jsx
+++ src/Components/Posts/Post.jsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom"
 
-const Post = ({ id, title, description }) => {
+const Post = ({ id, title, image, description }) => {
+    console.log(image === '');
     return (
         <div className="row">
             <div className="col-sm">
                 <div className="card my-4" style={{ width: "18rem" }}>
-                    <div className="card-body">
-                        <h3 className="card-title">{title}</h3>
-                        <p className="card-text">{description}</p>
-                        <Link to={"/post/" + id} >Lire l'article</Link>
+                    <div>
+                        {
+                            (image === ''||  image === null || typeof(image) === 'undefined' )?
+                            <img src='/image/pexels-andy-vu-3244513.jpg' alt="" style={{width: '100%' }} />
+                            :
+                            <img src={image} alt="" style={{width: '100%' }} />
+                        }
+                        <h3 className="card-title my-4 ml-2">{title}</h3>
+                        <p className="card-text my-4 ml-2">{description}</p>
+                        <p className="text-center"><Link to={"/post/" + id} ><span className="text-white btn btn-primary"> Lire l'article</span></Link></p>
+                        
                     </div>
                 </div>
             </div>
